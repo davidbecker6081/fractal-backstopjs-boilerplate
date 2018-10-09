66345b5f828a5aecef081b26501af7340861069e
davidbecker6081 <dbecker@peaksware.com>
2018-10-09 11:36:50 -0600

    Setup project for fractal and backstopjs testing

---
+++ backstop_data/html_report/config.js
@@ -0,0 +1,32 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_Fractal_0_document_0_phone.png",
+        "test": "..\\bitmaps_test\\20181009-113415\\backstop_default_Fractal_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "backstop_default_Fractal_0_document_0_phone.png",
+        "label": "Fractal",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://localhost:3000/components/detail/atomexample",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.31",
+          "analysisTime": 13
+        },
+        "diffImage": "..\\bitmaps_test\\20181009-113415\\failed_diff_backstop_default_Fractal_0_document_0_phone.png"
+      },
+      "status": "fail"
+    }
+  ],
+  "id": "backstop_default"
+});
