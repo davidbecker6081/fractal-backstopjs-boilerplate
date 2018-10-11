c244282d1d31947afff18341b40cd868492d7073
davidbecker6081 <dbecker@peaksware.com>
2018-10-11 15:35:14 -0600

    Fix url for test scenario within fractal

--- backstop_data/html_report/config.js
+++ backstop_data/html_report/config.js
@@ -3,29 +3,51 @@ report({
   "tests": [
     {
       "pair": {
-        "reference": "..\\bitmaps_reference\\backstop_default_Fractal_Example_0_document_0_phone.png",
-        "test": "..\\bitmaps_test\\20181010-095028\\backstop_default_Fractal_Example_0_document_0_phone.png",
-        "selector": "document",
-        "fileName": "backstop_default_Fractal_Example_0_document_0_phone.png",
+        "reference": "..\\bitmaps_reference\\backstop_default_Fractal_Example_0_test_0_desktop.png",
+        "test": "..\\bitmaps_test\\20181011-153040\\backstop_default_Fractal_Example_0_test_0_desktop.png",
+        "selector": ".test",
+        "fileName": "backstop_default_Fractal_Example_0_test_0_desktop.png",
         "label": "Fractal Example",
         "requireSameDimensions": true,
         "misMatchThreshold": 0.1,
         "url": "http://localhost:3000/components/detail/atomexample",
         "referenceUrl": "",
         "expect": 0,
-        "viewportLabel": "phone",
+        "viewportLabel": "desktop",
         "diff": {
           "isSameDimensions": true,
           "dimensionDifference": {
             "width": 0,
             "height": 0
           },
-          "misMatchPercentage": "0.31",
-          "analysisTime": 13
-        },
-        "diffImage": "..\\bitmaps_test\\20181010-095028\\failed_diff_backstop_default_Fractal_Example_0_document_0_phone.png"
+          "misMatchPercentage": "0.00"
+        }
       },
-      "status": "fail"
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_Selectors_0_SDkEP_0_desktop.png",
+        "test": "..\\bitmaps_test\\20181011-153040\\backstop_default_Selectors_0_SDkEP_0_desktop.png",
+        "selector": ".SDkEP",
+        "fileName": "backstop_default_Selectors_0_SDkEP_0_desktop.png",
+        "label": "Selectors",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://www.google.com",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
     }
   ],
   "id": "backstop_default"
